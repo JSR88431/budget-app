02e5d595fedcc6eebb1cbcd5687ffa402bf83dce
Justin Kooner <kooner92@gmail.com>
2018-10-04 14:47:17 -0700

    working modals

--- public/js/budget.js
+++ public/js/budget.js
@@ -1,4 +1,19 @@
-$("#create").click(function() {
+$("#create").click(function(event) {
+    event.preventDefault();
+    $(".modal").addClass("is-active");  
+  });
+  
+  $("#cancel").click(function() {
+    $(".modal").removeClass("is-active");  
+  });
+
+  $(".delete").click(function() {
+    $(".modal").removeClass("is-active");  
+  });
+
+
+$("#find").click(function(event) {
+  event.preventDefault();
     $(".modal").addClass("is-active");  
   });
   
